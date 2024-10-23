84c011d27258cf02e3198bf7aa233cec5d0c4da3
uesleijr <167311567+uesleijr@users.noreply.github.com>
2024-10-23 08:27:38 -0300

    fim aula

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js";
+import { getCSS, tickConfig } from "./common.js"
 
 async function quantidadeUsuarios() {
   const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -20,9 +20,35 @@ async function quantidadeUsuarios() {
 
   const layout = {
     plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+      },
+      xaxis: {
+        tickfont: tickConfig,
+          title: {
+              text: 'nome das redes sociais',
+              font: {
+                  color: getCSS('--secondary-color')
+              }
+          }
+      },
+      yaxis: {
+        tickfont: tickConfig,
+          title: {
+              text: 'bilhões de usuários ativos',
+              font: {
+                  color: getCSS('--secondary-color')
+              }
+          }
+      }
   }
-
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
